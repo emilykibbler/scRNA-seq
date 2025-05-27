5cfaf558c6e8ff2d408359dd56391e88725ba2f1
Emily Kibbler <emilykibbler@gmail.com>
2025-05-27 14:57:43 -0400

    Test connection, first push

---
+++ libraries.R
@@ -0,0 +1,10 @@
+install_packages <- function() {
+  install.packages("Seurat")
+  install.packages("tidyverse")
+
+}
+
+load_packages <- function() {
+  library("Seurat")
+  library("tidyverse")
+}
